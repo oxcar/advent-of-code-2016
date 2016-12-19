bcc967f13b42e5978a3514295cd527afdef2dfa8
Oscar Hernandez <oxcarh@gmail.com>
2016-12-19 04:32:43 -0600

    Day 19 - Solution 1

---
+++ src/main/scala/com/oxcarh/adventofcode2016/Day19.scala
@@ -0,0 +1,29 @@
+package com.oxcarh.adventofcode2016
+
+object Day19 extends App {
+
+  println("Day 19: An Elephant Named Joseph")
+
+  val numberOfElves = 3012210
+
+  val elves = Array.fill[Int](numberOfElves)(1)
+  while (elves.indexOf(numberOfElves) == -1) {
+    stealPresents(elves)
+  }
+  println(s"Solution 1: ${elves.indexOf(numberOfElves) + 1}")
+
+  def stealPresents(elves: Array[Int]): Unit = {
+    for (i <- elves.indices) {
+      if (elves(i) != 0) {
+        var j = if (i + 1 >= elves.length) 0 else i + 1
+        while (elves(j) == 0) {
+          j += 1
+          if (j >= elves.length) j = 0
+        }
+        elves(i) = elves(i) + elves(j)
+        elves(j) = 0
+      }
+    }
+  }
+
+}
