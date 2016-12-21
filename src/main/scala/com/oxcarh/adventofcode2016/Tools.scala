1204039fc74065c9df7a517854babb35ee8693fb
Oscar Hernandez <oxcarh@gmail.com>
2016-12-21 01:32:32 -0600

    …

--- src/main/scala/com/oxcarh/adventofcode2016/Tools.scala
+++ src/main/scala/com/oxcarh/adventofcode2016/Tools.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object Tools {
 
   def loadDayInputAsText(day: Int = 0): String = {
-    Source.fromInputStream(getClass.getResourceAsStream(s"/input_day$day.txt")).mkString
+    Source.fromInputStream(getClass.getResourceAsStream(s"/input_day_$day.txt")).mkString
   }
 
 }
