721d696fd9d37c63ab32df52a2951c523aa17ebe
Oscar Hernandez <oxcarh@gmail.com>
2016-12-21 03:58:23 -0600

    Day 3

--- src/main/scala/com/oxcarh/adventofcode2016/Day3.scala
+++ src/main/scala/com/oxcarh/adventofcode2016/Day3.scala
@@ -6,15 +6,21 @@ package com.oxcarh.adventofcode2016
 
 import com.oxcarh.adventofcode2016.Tools._
 
+import scala.util.matching.Regex
+
 object Day3 extends App {
 
   val input = loadDayInputAsText(3).split("\n")
 
+  implicit val linePattern = """[\s]*(\d+)[\s]*(\d+)[\s]*(\d+)[\s]*""".r
+
   // Solution 1 ------------------------------------------------------------
+
   val validTriangles = input.map(Triangle(_)).count(_.isValid)
   println(s"Solution 1: $validTriangles")
 
   // Solution 2 ------------------------------------------------------------
+
   val transposeInput = createTransposeInput(input)
   val validTriangles2 = transposeInput.foldLeft(0) { (validTriangles, row) =>
     row.grouped(3)
@@ -33,18 +39,16 @@ object Day3 extends App {
 
   object Triangle {
 
-    def apply(sides: String): Triangle = {
-      val pattern = """(\d+)[\s]*(\d+)[\s]*(\d+)""".r
-      val groups = pattern.findAllIn(sides)
-      new Triangle(groups.group(1).toInt, groups.group(2).toInt, groups.group(3).toInt)
+    def apply(sides: String)(implicit pattern: Regex): Triangle = {
+      sides match {
+        case pattern(a, b, c) => new Triangle(a.toInt, b.toInt, c.toInt)
+      }
     }
   }
 
-  def createTransposeInput(lines: Array[String]): Array[Array[Int]] = {
-    lines.map { line =>
-      val pattern = """(\d+)[\s]*(\d+)[\s]*(\d+)""".r
-      val groups = pattern.findAllIn(line)
-      Array(groups.group(1).toInt, groups.group(2).toInt, groups.group(3).toInt)
+  def createTransposeInput(lines: Array[String])(implicit pattern: Regex): Array[Array[Int]] = {
+    lines.map {
+      case pattern(a, b, c) => Array(a.toInt, b.toInt, c.toInt)
     }.transpose
   }
 
